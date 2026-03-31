133732e02813890f23ea1a8aa0292ef628380e9c
Shakib Khan <leadwithshakib@gmail.com>
2026-03-31 08:28:21 +0600

    chore: remove Jest and Playwright testing configurations and dependencies

--- __tests__/e2e/home.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("home page should have correct heading", async ({ page }) => {
-  await page.goto("/");
-  // Checking for the big heading in the landing page
-  const h1 = page.locator("h1");
-  await expect(h1).toContainText("EXPLORE YOUR");
-  await expect(h1).toContainText("CODEBASE.");
-});
